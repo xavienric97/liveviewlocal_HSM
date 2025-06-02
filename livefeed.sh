695c57945cb621162516da32ac851891305f7598
xavienric97 <89940152+xavienric97@users.noreply.github.com>
2025-06-02 11:48:25 -0500

    Update livefeed.sh

--- livefeed.sh
+++ livefeed.sh
@@ -6,4 +6,4 @@ export DISPLAY=:0
 # Run Livefeed
 #timeout es cuanto tiempo encendido estará el feed (en segundos, ej. 300s es 5min)
 # 1 5 3 significa: (1 payload) (5 calidad max) (3 camara IR)
-timeout 300 /home/$USER/Edge-SDK/build/bin/test_liveview 1 5 3
+timeout 1320 /home/$USER/Edge-SDK/build/bin/test_liveview 1 5 3
